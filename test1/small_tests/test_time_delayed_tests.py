4e02363d5b8a0c5b6ca810d5701a8e1ec6b98e51
Gattamaneni Kumar Raja <rajasvu@gmail.com>
2022-11-27 19:16:21 +0530

    Elevated wait times to check parallel execution benefits

--- test1/small_tests/test_time_delayed_tests.py
+++ test1/small_tests/test_time_delayed_tests.py
@@ -4,8 +4,8 @@ from unittest import TestCase
 
 class TestParallelExecution(TestCase):
     def test_wait_for_one_second(self):
-        sleep(1)
+        sleep(10)
         self.assertFalse(1 == 2)
     def test_wait_for_three_seconds(self):
-        sleep(3)
+        sleep(10)
         self.assertFalse(1 == 2)
